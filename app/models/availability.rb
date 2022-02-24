74a38f7b359454a14a0ca394a2ade0faae3d8f86
stephanaltmuller <stephan.altmuller@gmail.com>
2022-02-24 11:50:39 +0100

    pgsearch

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,4 +1,13 @@
 class Availability < ApplicationRecord
   belongs_to :beast
   validates :start_date, :end_date, presence: true
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :start_date, :end_date ],
+    associated_against: {
+      beast: [ :name, :species, :gender, :bio ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
