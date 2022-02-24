984053a8dce4ae30e5997eafb0b495e21ae5a90f
Vincent Sengel <vincent.sengel@gmail.com>
2022-02-24 23:17:44 +0100

    updated user model to accomodate pictures and fixed alignment and font on dropdown menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :bio, :photo])
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :bio, :photo])
   end
 end
